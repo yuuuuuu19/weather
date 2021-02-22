568fdb5bbf5a4f9d69b47003417075313b2e13c2
山本祐樹 <20aw0140@jec.ac.jp>
2021-02-22 22:29:55 +0900

    1st

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,182 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="main" :class="isDay ? 'day' : 'night'">
+    <div class="container my-5">
+      <h1 class="title text-center">天気予報</h1>
+      <form class="search-location" v-on:submit.prevent="getWeather">
+        <input
+          type="text"
+          class="form-control text-muted form-rounded p-4 shadow-sm"
+          placeholder="都市名を検索"
+          v-model="citySearch"
+          autocomplete="off"
+        />
+      </form>
+
+
+
+
+        <div class="card-top text-center" style="margin-bottom: 12rem">
+          <div class="city-name my-3">
+            <p>{{ weather.cityName }}</p>
+            <span>...</span>
+            <p class="">{{ weather.country }}</p>
+          </div>
+        </div>
+
+      
+
+        <div class="card-body">
+
+          <div class="card-mid">
+            <div class="row">
+              <div class="col-12 text-center temp">
+                <span>{{ weather.temperature }}&deg;C</span>
+                <p class="my-4">{{ weather.description }}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col d-flex justify-content-between px-5 mx-5">
+                <p>
+                  <img src="./assets/down.svg" alt="" />
+                  {{ weather.lowTemp }}&deg;C
+                </p>
+                <p>
+                  <img src="./assets/up.svg" alt="" />
+                 {{ weather.highTemp }}&deg;C
+                </p>
+              </div>
+            </div>
+          </div>
+        
+          <div class="card-bottom px-5 py-4 row">
+            <div class="col text-center">
+              <p>{{ weather.feelsLike }}&deg;C</p>
+              <span>Feels like</span>
+            </div>
+            <div class="col text-center">
+              <p>{{ weather.humidity }}%</p>
+              <span>humidity</span>
+            </div>
+          </div>
+
+  
+        </div>
+      </div>
+    </div>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script>
+ export default{
+   data(){
+     return{
+     cityFound: true,
+      visible: false,
+      stormy: false,
+      cloudy: false,
+      clearSky: false,
+      clearNight: false,
+      snowy: false,
+      isDay: true,
+      citySearch: "",
+      weather: {
+        cityName:"",
+        country: "",
+        temperature: "",
+        description: "",
+        lowTemp: "",
+        highTemp: "",
+        feelsLike: "",
+        humidity: "",
+       }
+     };
+   },
+    methods: {
+    getWeather: async function () {
+      console.log(this.citySearch);
+      const key = "a023881ec689a57927b5968c954edfcf";
+      const baseURL = `http://api.openweathermap.org/data/2.5/weather?q=${this.citySearch}&appid=${key}&units=metric`;
+    
+      try {
+        const response = await fetch(baseURL);
+        const data = await response.json();
+        console.log(data);
+        this.citySearch = "";
+        this.weather.cityName = data.name;
+        this.weather.country = data.sys.country;
+        this.weather.temperature = Math.round(data.main.temp);
+        this.weather.description = data.weather[0].description;
+        this.weather.lowTemp = Math.round(data.main.temp_min);
+        this.weather.highTemp = Math.round(data.main.temp_max);
+        this.weather.feelsLike = Math.round(data.main.feels_like);
+        this.weather.humidity = Math.round(data.main.humidity);
+        const timeOfDay = data.weather[0].icon;
+      
+        if (timeOfDay.includes("n")) {
+          this.isDay = false;
+        } else {
+          this.isDay = true;
+        }
+        const mainWeather = data.weather[0].main;
+  
+        if (mainWeather.includes("Clouds")) {
+          this.stormy = false;
+          this.cloudy = true;
+          this.clearSky = false;
+          this.clearNight = false;
+          this.snowy = false;
+        }
+        if (mainWeather.includes("Clouds")) {
+          this.stormy = false;
+          this.cloudy = true;
+          this.clearSky = false;
+          this.clearNight = false;
+          this.snowy = false;
+        }
+        if (
+          mainWeather.includes("Thunderstorm") ||
+          mainWeather.includes("Rain")
+        ) {
+          this.stormy = true;
+          this.cloudy = false;
+          this.clearSky = false;
+          this.clearNight = false;
+          this.snowy = false;
+        }
+        if (mainWeather.includes("Clear") && this.isDay) {
+          this.stormy = false;
+          this.cloudy = false;
+          this.clearSky = true;
+          this.clearNight = false;
+          this.snowy = false;
+        }
+        if (mainWeather.includes("Clouds") && !this.isDay) {
+          this.stormy = false;
+          this.cloudy = false;
+          this.clearSky = false;
+          this.clearNight = true;
+          this.snowy = false;
+        }
+        if (mainWeather.includes("Snow")) {
+          this.stormy = false;
+          this.cloudy = false;
+          this.clearSky = false;
+          this.clearNight = false;
+          this.snowy = true;
+        }
+        this.visible = true;
+        this.cityFound = false;
+      } catch (error) {
+        console.log(error);
+        this.cityFound = true;
+        this.visible = false;
+      }
+    },
+    },
+ };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+@import "./assets/custom.css";
 </style>
